d2c3db8e6926deb315c7250a13e91e2e52f23a13
gavinsharkey <gavinsharkey2016@gmail.com>
2024-08-14 19:52:21 -0500

    Add post function for actions

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -20,3 +20,25 @@ export async function fetcher<T>(key: string): Promise<T> {
 
   return json;
 }
+
+export async function post<T>(
+  path: string,
+  body: Record<string, any>
+): Promise<T> {
+  const resp = await fetch(BASE_URL + path, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  const json = await resp.json();
+
+  if (!resp.ok) {
+    throw new Error(json?.message || resp.statusText);
+  }
+
+  return json;
+}

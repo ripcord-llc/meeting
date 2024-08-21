883cf1c9b80a68543b31a4c54e923855624a6853
gavinsharkey <gavinsharkey2016@gmail.com>
2024-08-21 14:56:41 -0500

    Add findLead action

--- src/api/deals/index.ts
+++ src/api/deals/index.ts
@@ -1,3 +1,22 @@
+import { SWRConfiguration } from 'swr';
+import useSWRImmutable from 'swr/immutable';
+
+import { post } from '../fetcher';
+
+import { LeadInjectionResponse } from './types';
+
 export const PUBLIC_DEALS_ENDPOINTS = {
+  findLead: '/public/deals/find-lead',
   injectLead: '/public/deals/inject-lead',
 };
+
+export function useFindLead(
+  params?: { routingId?: string; productId?: string; email?: string } | null,
+  config?: SWRConfiguration<LeadInjectionResponse>
+) {
+  return useSWRImmutable(
+    params?.email ? [PUBLIC_DEALS_ENDPOINTS.findLead, params] : null,
+    async (key) => post<LeadInjectionResponse>(key[0], key[1] as Record<string, any>),
+    config
+  );
+}

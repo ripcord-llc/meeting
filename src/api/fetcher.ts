6843f4a78a95e437d3936a6a8c47d036a56cec02
gavinsharkey <gavinsharkey2016@gmail.com>
2024-08-21 22:06:12 -0500

    Add Exception class for server errors, and update fetcher functions to throw them

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,35 +1,44 @@
 const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://api.ripcord.io";
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api.ripcord.io';
+
+export class Exception extends Error {
+  constructor(
+    public response: {
+      message: string;
+      error: string;
+      statusCode: number;
+    }
+  ) {
+    super(response.error);
+
+    this.name = 'Exception';
+  }
+}
 
 export async function fetcher<T>(key: string): Promise<T> {
   const resp = await fetch(BASE_URL + key, {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
   });
 
   const json = await resp.json();
 
   if (!resp.ok) {
-    throw new Error(json?.message || resp.statusText);
+    throw new Exception(json);
   }
 
   return json;
 }
 
-export async function post<T>(
-  path: string,
-  body: Record<string, any>
-): Promise<T> {
+export async function post<T>(path: string, body: Record<string, any>): Promise<T> {
   const resp = await fetch(BASE_URL + path, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
     body: JSON.stringify(body),
   });
@@ -37,7 +46,7 @@ export async function post<T>(
   const json = await resp.json();
 
   if (!resp.ok) {
-    throw new Error(json?.message || resp.statusText);
+    throw new Exception(json);
   }
 
   return json;

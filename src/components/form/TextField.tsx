9d03fa53b5d1b509fd44280d8a0b5a1708aa1ee0
gavinsharkey <gavinsharkey2016@gmail.com>
2025-06-08 16:30:13 -0500

    Strip company url field of protocol on paste

--- src/components/form/TextField.tsx
+++ src/components/form/TextField.tsx
@@ -1,4 +1,6 @@
+import { useCallback } from 'react';
 import MuiTextField, { TextFieldProps } from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
 
 import { useFormContext, Controller } from 'react-hook-form';
 
@@ -26,6 +28,62 @@ export default function TextField({ name, helperText, label, description, ...oth
           helperText={error ? error?.message : helperText}
           inputProps={{
             ref,
+          }}
+          {...other}
+        />
+      )}
+    />
+  );
+}
+
+export const removeProtocol = (url: string) => url.replace(/^https?:\/\//, '');
+
+export function UrlTextField({ name, helperText, InputProps, inputProps, ...other }: Props) {
+  const { control, setValue, getValues } = useFormContext();
+
+  const handlePaste = useCallback(
+    (event: React.ClipboardEvent) => {
+      // Strips urls of their protocol before pasting. Useful since most people will copy the urls from their browser
+      const currentValue = getValues(name);
+
+      if (currentValue) return;
+
+      const paste = event.clipboardData.getData('text');
+
+      if (paste) {
+        setValue(name, removeProtocol(paste), {
+          shouldValidate: true,
+        });
+
+        event.preventDefault();
+      }
+    },
+    [name, getValues, setValue]
+  );
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { ref, ...field }, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          fullWidth
+          value={typeof field.value === 'number' && field.value === 0 ? '' : field.value}
+          error={!!error}
+          helperText={error ? error?.message : helperText}
+          inputProps={{
+            ref,
+            ...inputProps,
+          }}
+          onPaste={handlePaste}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start" sx={{ mr: 0 }}>
+                https://
+              </InputAdornment>
+            ),
+            ...InputProps,
           }}
           {...other}
         />
